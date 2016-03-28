b15e126f9b92138d270d4b9cf6faf6d4af956500
pantol <pantolik@rambler.ru>
2016-03-28 23:59:24 +0300

    Сделал объекты но не полностью

--- script2.js
+++ script2.js
@@ -47,18 +47,36 @@ function ChessTable()
         }
     }
 }
+var selectedTd;
+function highlight(node) 
+{
+  if (selectedTd) {
+    selectedTd.classList.remove('highlight');
+  }
+    selectedTd = node;
+    selectedTd.classList.add('highlight');
+}
+
+var lit = ['0','A','B','C','D','E','F','G','H'];
+function printSelected(){
+    var r = selectedTd.parentElement.rowIndex;
+    var c = selectedTd.cellIndex;
+    var cl = lit[c];
+    document.getElementById('log').innerHTML+=' ' + cl + r + ', ';   
+}
 
 //Объект Поле
 function Field(node){
     var myTable = document.getElementById('Mytable');
-    var selectedTd;
+    //var selectedTd;
     var lit = ['0','A','B','C','D','E','F','G','H'];
-    //var r = node.parrentElement.rowIndex;
-    //var c = node.cellIndex;
-    
-    this.hightlight = function (node) 
+    if (selectedTd){
+    //var r = selectedTd.parentElement.rowIndex;
+    //var c = selectedTd.cellIndex;
+    }
+    this.hightlight = function () 
     {
-      if (selectedTd) {
+        if (selectedTd) {
         selectedTd.classList.remove('highlight');
       }
         selectedTd = node;
@@ -66,48 +84,48 @@ function Field(node){
     }
 
     this.print = function (){
-        var r = selectedTd.parentElement.rowIndex;
-        var c = selectedTd.cellIndex;
+        var r = node.parentElement.rowIndex;
+        var c = node.cellIndex;
         var cl = lit[c];
         document.getElementById('log').innerHTML += cl + r + ', ';   
     }
     this.toLeft = function (){
-        var r = node.parrentElement.rowIndex;
+        var r = node.parentElement.rowIndex;
         var c = node.cellIndex;
         var cnew = c - 1;
         if(c <= 1){cnew = 1}
         var newCell = myTable.rows[r].cells[cnew];
-        this.highlight(newCell);
-        if(c > 1)this.print();
+        highlight(newCell);
+        if(c > 1)printSelected();
     }
     this.toUp = function (){
-        var r = node.parrentElement.rowIndex;
+        var r = node.parentElement.rowIndex;
         var c = node.cellIndex;
-            var rnew = r - 1;
-            if(r <= 1){rnew = 1}
-            var newCell = myTable.rows[rnew].cells[c];
-            highlight(newCell);
-            if(r > 1)printSelected();        
+        var rnew = r - 1;
+        if(r <= 1){rnew = 1}
+        var newCell = myTable.rows[rnew].cells[c];
+        highlight(newCell);
+        if(r > 1)printSelected();        
     }
     this.toRight = function (){
-        var r = node.parrentElement.rowIndex;
+        var r = node.parentElement.rowIndex;
         var c = node.cellIndex;
-            var cnew = c + 1;
-            if(c >= 8){cnew = 8}
-            var newCell = myTable.rows[r].cells[cnew];
-            highlight(newCell);
-            var cl = lit[cnew];
-            if(c < 8)printSelected();       
+        var cnew = c + 1;
+        if(c >= 8){cnew = 8}
+        var newCell = myTable.rows[r].cells[cnew];
+        highlight(newCell);
+        var cl = lit[cnew];
+        if(c < 8)printSelected();       
     }
     this.toDown = function (){
-        var r = node.parrentElement.rowIndex;
+        var r = node.parentElement.rowIndex;
         var c = node.cellIndex;
-            var rnew = r + 1;
-            if(r >= 8){rnew = 8}
-            var newCell = myTable.rows[rnew].cells[c];
-            highlight(newCell);
-            var cl = lit[c];
-            if(r < 8)printSelected();        
+        var rnew = r + 1;
+        if(r >= 8){rnew = 8}
+        var newCell = myTable.rows[rnew].cells[c];
+        highlight(newCell);
+        var cl = lit[c];
+        if(r < 8)printSelected();        
     }
     
 
@@ -118,21 +136,20 @@ window.onload = function()
     var myChess = new ChessTable();
     myChess.create();
     var node;
-    //var myField = new Field(node);
+    var myField = new Field(node);
     var myTable = document.getElementById('Mytable');
     myTable.onclick = function(event)
     {
       var target = event.target;
 
       while (target != this) {
-        var r = target.parentElement.rowIndex;
-        var c = target.cellIndex;
-        var myField = new Field(target);  
-          //alert(r+"  :"+c);
-          //alert((target.tagName == 'TD') && ( (r>0) && (c>0)));
+          var r = target.parentElement.rowIndex;
+          var c = target.cellIndex;
+          var myField = new Field(target);
+          
         if (target.tagName == 'TD' && r>0 && c>0 )
         {
-            myField.hightlight(target);
+            myField.hightlight();
             myField.print();
           return;
         }
@@ -140,6 +157,7 @@ window.onload = function()
       }
     }
     window.onkeydown=function(event){
+        var myField = new Field (selectedTd);
 
         switch(event.keyCode){
             case 37:
@@ -157,93 +175,3 @@ window.onload = function()
         }
     }    
 }
-
-
-/*
-
-var myTable = document.getElementById('Mytable');
-var selectedTd;
-var lit = ['0','A','B','C','D','E','F','G','H'];
-
-myTable.onclick = function(event)
-{
-  var target = event.target;
-
-  while (target != this) {
-    var r = target.parentElement.rowIndex;
-    var c = target.cellIndex;
-      //alert(r+"  :"+c);
-      //alert((target.tagName == 'TD') && ( (r>0) && (c>0)));
-    if (target.tagName == 'TD' && r>0 && c>0 )
-    {
-        highlight(target);
-        printSelected();
-      return;
-    }
-    target = target.parentNode;
-  }
-}
-
-function highlight(node) 
-{
-  if (selectedTd) {
-    selectedTd.classList.remove('highlight');
-  }
-    selectedTd = node;
-    selectedTd.classList.add('highlight');
-}
-
-function printSelected(){
-    var r = selectedTd.parentElement.rowIndex;
-    var c = selectedTd.cellIndex;
-    var cl = lit[c];
-    document.getElementById('log').innerHTML+=' ' + cl + r + ', ';   
-}
-
-window.onkeydown=function(event){
-    switch(event.keyCode){
-        case 37:
-            var r = selectedTd.parentElement.rowIndex;
-            var c = selectedTd.cellIndex;
-            var cnew = c - 1;
-            if(c <= 1){cnew = 1}
-            var newCell = myTable.rows[r].cells[cnew];
-            highlight(newCell);
-            if(c > 1)printSelected();
-            break;
-        case 38:
-            var r = selectedTd.parentElement.rowIndex;
-            var c = selectedTd.cellIndex;
-            var rnew = r - 1;
-            if(r <= 1){rnew = 1}
-            var newCell = myTable.rows[rnew].cells[c];
-            highlight(newCell);
-            if(r > 1)printSelected();
-            break;
-        case 39:
-            var r = selectedTd.parentElement.rowIndex;
-            var c = selectedTd.cellIndex;
-            var cnew = c + 1;
-            if(c >= 8){cnew = 8}
-            var newCell = myTable.rows[r].cells[cnew];
-            highlight(newCell);
-            var cl = lit[cnew];
-            if(c < 8)printSelected();
-            break;
-        case 40:
-            var r = selectedTd.parentElement.rowIndex;
-            var c = selectedTd.cellIndex;
-            var rnew = r + 1;
-            if(r >= 8){rnew = 8}
-            var newCell = myTable.rows[rnew].cells[c];
-            highlight(newCell);
-            var cl = lit[c];
-            if(r < 8)printSelected();
-            break;            
-    }
-}
-
-
-
-
-*/
